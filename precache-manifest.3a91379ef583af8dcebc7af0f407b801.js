7196d2e8138a2856d3764062cd528fd1b738300b
zey3830 <56261624+zey3830@users.noreply.github.com>
2020-02-23 12:28:17 -0500

    Updates

---
+++ precache-manifest.3a91379ef583af8dcebc7af0f407b801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa60658db88ab062bc65bc9ea10c599",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "52294037aba065277aa3",
+    "url": "/react_test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b4c65e065c86b601519c",
+    "url": "/react_test/static/js/2.eb0c6c59.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_test/static/js/2.eb0c6c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52294037aba065277aa3",
+    "url": "/react_test/static/js/main.182c8d65.chunk.js"
+  },
+  {
+    "revision": "c1cd8733297d166f8e7c",
+    "url": "/react_test/static/js/runtime-main.6e54df9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
